4ca0e2505d3a90f63aa1148fd79659b880997dcf
SinirliJapon <mkemalerylmz@gmail.com>
2025-02-22 20:03:01 +0100

    add new util methods

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -3,4 +3,22 @@ abstract class Utils {
     if (text.isEmpty) return text;
     return text[0].toUpperCase() + text.substring(1);
   }
+
+  static String getUnitSymbol(String unit) {
+    if (unit == "imperial") {
+      return "°F";
+    } else if (unit == "metric") {
+      return "°C";
+    }
+    return "°K";
+  }
+
+  static String getWindSpeedUnit(String unit) {
+    if (unit == "imperial") {
+      return "mph";
+    } else if (unit == "metric") {
+      return "m/s";
+    }
+    return "m/s";
+  }
 }
