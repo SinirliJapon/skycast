5c15fb9887452a1c77e7aabd307b15f710aa8fbd
SinirliJapon <mkemalerylmz@gmail.com>
2025-02-25 19:55:55 +0100

    apply rounding to temperature values

--- lib/widgets/custom_forecast_info_column.dart
+++ lib/widgets/custom_forecast_info_column.dart
@@ -12,16 +12,16 @@ class CustomForecastInfoColumn extends StatelessWidget {
   Widget build(BuildContext context) {
     String temperatureUnit = Utils.getUnitSymbol(unit);
     String windSpeedUnit = Utils.getWindSpeedUnit(unit);
+    final temperature = weather.temperature.round();
+    final feelsLike = weather.feelsLike.round();
+    final tempMin = weather.tempMin.round();
+    final tempMax = weather.tempMax.round();
     return Column(
       children: [
-        CustomListTile(
-          title: "Temperature",
-          icon: Icons.device_thermostat,
-          value: "${weather.temperature}$temperatureUnit",
-        ),
-        CustomListTile(title: "Feels Like", icon: Icons.thermostat, value: "${weather.feelsLike}$temperatureUnit"),
-        CustomListTile(title: "Min Temperature", icon: Icons.thermostat, value: "${weather.tempMin}$temperatureUnit"),
-        CustomListTile(title: "Max Temperature", icon: Icons.thermostat, value: "${weather.tempMax}$temperatureUnit"),
+        CustomListTile(title: "Temperature", icon: Icons.device_thermostat, value: "$temperature$temperatureUnit"),
+        CustomListTile(title: "Feels Like", icon: Icons.thermostat, value: "$feelsLike$temperatureUnit"),
+        CustomListTile(title: "Min Temperature", icon: Icons.thermostat, value: "$tempMin$temperatureUnit"),
+        CustomListTile(title: "Max Temperature", icon: Icons.thermostat, value: "$tempMax$temperatureUnit"),
         CustomListTile(title: "Wind Speed", icon: Icons.wind_power, value: "${weather.windSpeed} $windSpeedUnit"),
         CustomListTile(title: "Humidity", icon: Icons.water_drop, value: "${weather.humidity}%"),
         CustomListTile(title: "Pressure", icon: Icons.air, value: "${weather.pressure} hPa"),
